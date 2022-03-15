e4c685a1d0655e1f23bb894ee8c22e7388d7bac4
HenryFerreira <henryferreira133@gmail.com>
2022-03-15 15:11:03 -0300

    Se desarrollo el Test de la plataforma

--- src/main/java/co/com/sofka/app/calculator/BasicCalculator.java
+++ src/main/java/co/com/sofka/app/calculator/BasicCalculator.java
@@ -1,2 +1,14 @@
-package co.com.sofka.app.calculator;public class BasicCalculator {
+package co.com.sofka.app.calculator;
+
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+public class BasicCalculator {
+    private static final Logger logger = LoggerFactory.getLogger(BasicCalculator.class);
+
+    public Long sum(Long number1, Long number2) {
+        logger.info("Summing {} + {}", number1, number2);
+        return number1 + number2;
+    }
 }
--- src/test/java/co/com/sofka/app/calculator/BasicCalculatorTest.java
+++ src/test/java/co/com/sofka/app/calculator/BasicCalculatorTest.java
@@ -1,4 +1,26 @@
+package co.com.sofka.app.calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class BasicCalculatorTest {
-  
+    private final BasicCalculator basicCalculator = new BasicCalculator();
+
+    @DisplayName("Testing several sums")
+    @ParameterizedTest(name = "{0} + {1} = {2}")
+    @CsvSource({
+            "1,    1,   2",
+            "1,    2,   3",
+            "49,  51, 100",
+            "1,  100, 101"
+    })
+    public void severalSums(Long first, Long second, Long expectedResult) {
+        System.out.println(first + "|" + second + "|" + expectedResult);
+        assertEquals(expectedResult, basicCalculator.sum(first, second),
+                () -> first + " + " + second + " should equal " + expectedResult);
+    }
 }
